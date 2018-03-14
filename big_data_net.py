d6ae5ebb2915fd49b140cc86dcfefad5e9dd3559
liam-mitchell21 <liam.mitchell@uleth.ca>
2018-03-14 15:07:25 -0600

    updated thing to sucessfully convert matlab object into numpy array

--- big_data_net.py
+++ big_data_net.py
@@ -1,15 +1,12 @@
 
-
 import numpy
 import scipy.io
 
-param = scipy.io.whosmat('data_allSensors.mat')
+param = scipy.io.whosmat('dont_commit_this/3d_sensor_epochs.mat')
 print(param) #prints info about mat file we are reading in
 
-mat_contents = scipy.io.loadmat('data_allSensors.mat')
-matrices = numpy.array(mat_contents.values())
-	#converts from a dict_values object into a numpy matrix
-print(type(matrices))
-	#checks that we successfully converted obj types
+mat_contents = scipy.io.loadmat('dont_commit_this/3d_sensor_epochs.mat')
 
-print(dir(mat_contents))
+array_std = numpy.array(mat_contents['epoch_std'])
+array_trg = numpy.array(mat_contents['epoch_trg'])
+
