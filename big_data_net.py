3f443c4dc7bdb385a0d3df42c4584fa86da75038
Liam R. Mitchell <liam.mitchell@uleth.ca>
2018-03-15 23:26:32 -0600

    added some code to network can be modified to take differing numbers of trials. also added accuracy printout

--- big_data_net.py
+++ big_data_net.py
@@ -6,6 +6,9 @@
 
 import numpy
 import scipy.io
+import math
+import sklearn
+from sklearn import svm
 
 param = scipy.io.whosmat('dont_commit_this/2d_sensor_epochs.mat')
 #print(param) #prints info about mat file we are reading in
@@ -20,30 +23,31 @@ trg_dat = array_trg[:,:]
 
 sens_dat = numpy.concatenate((std_dat,trg_dat),axis=0)
 
-std_class = numpy.full((153*129,1),0)
-trg_class = numpy.full((147*129,1),1)
+htrials = 20
+trainsize = math.floor(htrials*129*(9/10))
+
+std_class = numpy.full((htrials*129,1),0)
+trg_class = numpy.full((htrials*129,1),1)
 sens_class = numpy.concatenate((std_class,trg_class),axis=0)
 
 #############
 # shufflin' #
 #############
 
-rand_key = numpy.random.permutation(300*129)
+rand_key = numpy.random.permutation(htrials*2*129)
 numpy.subtract(rand_key,1)
 #print(rand_key)
 sens_dat = sens_dat[rand_key,:]
 sens_class = sens_class[rand_key,:]
-print(sens_dat.shape)
-print(sens_class.shape)
+#print(sens_dat.shape)
+#print(sens_class.shape)
 
 ##################
 # defining model #
 ##################
 
-from sklearn import svm
-
 clf = svm.SVC(gamma=0.001, C=100.0)
-clf.fit(sens_dat[:-300,:], sens_class[:-300])
+clf.fit(sens_dat[:trainsize,:], sens_class[:trainsize])
 svm.SVC(C=100.0, cache_size=200, class_weight=None, coef0=0.0,
  decision_function_shape='ovr', degree=3, gamma=0.001, kernel='rbf',
  max_iter=-1, probability=False, random_state=None, shrinking=True,
@@ -53,8 +57,10 @@ svm.SVC(C=100.0, cache_size=200, class_weight=None, coef0=0.0,
 # making predictions #
 ######################
 
-print(numpy.transpose(sens_class[-300:,:]))
-print(clf.predict(sens_dat[-300:,:]))
+true_class = sens_class[trainsize:]
+predict_class = clf.predict(sens_dat[trainsize:])
+accuracy = sklearn.metrics.accuracy_score(true_class,predict_class)
+print(accuracy)
 
 ####################
 # persisting model #
